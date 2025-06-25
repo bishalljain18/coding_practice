1cdec66329434b6291ced8406342f59e6b268210
Bishal <bishaljain18@gmail.com>
2025-06-25 15:31:12 +0530

    bubble sort

--- bubble-sort.ts
+++ bubble-sort.ts
@@ -1,4 +1,4 @@
-const bubbleSort = (arr: any[]) => {
+const bubbleSort = (arr: []) => {
     for(let i=0; i<arr.length; i++) {
         for(let j=0; j<arr.length-1-i; j++) {
             if(arr[j] > arr[j+1]) {
